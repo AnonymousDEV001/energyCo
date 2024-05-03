8d404a8d2dea7b4ab59fbb12f669ed725ed08700
AnonymousDEV001 <126903535+AnonymousDEV001@users.noreply.github.com>
2024-05-03 04:11:51 -0700

    fixed nav issue

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 
 
 const Navbar = () => {
-  const [navToggle,setNavToggle] = useState(true)
+  const [navToggle,setNavToggle] = useState(false)
   return (
     <nav class="bg-white p-2 border-b-[1px] border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -14,7 +14,7 @@ const Navbar = () => {
           <span class="self-center text-5xl font-bold whitespace-nowrap dark:text-white">ENERGY<span className="text-red-600">CO</span></span>
         </a>
         <button
-        onClick={()=>setNavToggle(!navToggle)}
+        onClick={()=>{setNavToggle(!navToggle)}}
           data-collapse-toggle="navbar-default"
           type="button"
           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -38,7 +38,7 @@ const Navbar = () => {
             />
           </svg>
         </button>
-        <div class={`${navToggle && "hidden"} w-full md:block md:w-auto" id="navbar-default`}>
+        <div style={navToggle ? {display:"none"}: {display:"block"}} class="w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border text-gray-500 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
