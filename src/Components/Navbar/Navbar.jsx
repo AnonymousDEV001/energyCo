c778cf215cca486f444906ecefc762003571dd1b
AnonymousDEV001 <126903535+AnonymousDEV001@users.noreply.github.com>
2024-05-03 05:31:39 -0700

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             />
           </svg>
         </button>
-        <div style={navToggle ? {display:"none"}: {display:"block"}} className="w-full md:block md:w-auto" id="navbar-default">
+        <div style={navToggle ? {display:"block"}: {display:"none"}} className="w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border text-gray-500 border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
