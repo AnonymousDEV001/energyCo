8bc7afa119a1721b6ef904960350c8c97f49fc72
AnonymousDEV001 <126903535+AnonymousDEV001@users.noreply.github.com>
2024-05-03 04:44:28 -0700

    adding google manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from '@next/third-parties/google'
 import "./output.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="AW-11359687580" />
       <body className={inter.className}>{children}</body>
     </html>
   );
